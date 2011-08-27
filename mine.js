a66129ffd77eacab22482b513889c8291d42f7bc
Will <wstubbs@gmail.com>
2011-08-26 22:00:32 -0700

    Added uuid to game state, for later stashing

--- mine.js
+++ mine.js
@@ -1,18 +1,21 @@
+var uuid = require('node-uuid');
+
 var MineSweeper = exports.MineSweeper = function() {
-    this.width = 10;
-    this.height = 10;
-    this.board = new Array(this.height);
-    for (i=0;i<this.height;i++) {
-      this.board[i] = new Array(this.width);
-      for (j=0;j<this.width;j++) {
-        this.board[i][j] = MineSweeper.EMPTY;
-      }
+  this.uuid = uuid();
+  this.width = 10;
+  this.height = 10;
+  this.board = new Array(this.height);
+  for (i=0;i<this.height;i++) {
+    this.board[i] = new Array(this.width);
+    for (j=0;j<this.width;j++) {
+      this.board[i][j] = MineSweeper.EMPTY;
     }
-    var placed = 0;
-    while (placed<MineSweeper.BOMB_COUNT) {
-      this.placeMine(this.random(this.height),this.random(this.width));
-      placed++;
-    }
+  }
+  var placed = 0;
+  while (placed<MineSweeper.BOMB_COUNT) {
+    this.placeMine(this.random(this.height),this.random(this.width));
+    placed++;
+  }
 };
 
 MineSweeper.prototype.placeMine = function(x, y) {
