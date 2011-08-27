47eb5d00a5482091739cc6a1f09e4d1c3cd2453a
Will <wstubbs@gmail.com>
2011-08-27 15:04:39 -0700

    now less casual ;)

--- mine.js
+++ mine.js
@@ -140,7 +140,7 @@ MineSweeper.prototype.over = function() {
     return (this.revealed + MineSweeper.BOMB_COUNT) >= (this.width*this.height);
 }
 
-MineSweeper.BOMB_COUNT = 0;
+MineSweeper.BOMB_COUNT = 15;
 MineSweeper.EMPTY = 0;
 MineSweeper.BOMB = -1;
 MineSweeper.FLAG = -2;
